92aa9d24e9411dfd0da64a0ace0afc3ec04670c7
alievaleila <meliyeva452@gmail.com>
2025-12-10 01:53:21 +0400

    initial commit

---
+++ src/main/java/az/edu/itbrains/furni_online_store/dtos/ProductDto.java
@@ -0,0 +1,17 @@
+package az.edu.itbrains.furni_online_store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String imageUrl;
+    private String name;
+    private Double price;
+}
---
+++ src/main/java/az/edu/itbrains/furni_online_store/services/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package az.edu.itbrains.furni_online_store.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+}
