675fa7386bf50ece9d533a041b39271f413ffa63
alievaleila <meliyeva452@gmail.com>
2025-12-09 23:39:10 +0400

    initial commit

---
+++ src/test/java/az/edu/itbrains/furni_online_store/FurniOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.itbrains.furni_online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
