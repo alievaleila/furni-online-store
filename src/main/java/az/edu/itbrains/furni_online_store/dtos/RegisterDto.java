614c1d910f1bc52f9da50583fa03b2eeec289862
alievaleila <meliyeva452@gmail.com>
2025-12-12 23:58:17 +0400

    initial commit

--- src/main/java/az/edu/itbrains/furni_online_store/dtos/RegisterDto.java
+++ src/main/java/az/edu/itbrains/furni_online_store/dtos/RegisterDto.java
@@ -10,5 +10,9 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterDto {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
 
 }
--- src/main/java/az/edu/itbrains/furni_online_store/models/Cart.java
+++ src/main/java/az/edu/itbrains/furni_online_store/models/Cart.java
@@ -1,4 +1,29 @@
 package az.edu.itbrains.furni_online_store.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="cart")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String imageUrl;
+    private String name;
+    private Double price;
+    private String quantity;
+
+
 }
