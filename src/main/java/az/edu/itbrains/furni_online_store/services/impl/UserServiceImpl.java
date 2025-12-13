a3f58e76dc455e70273d0531dd394fac75323fe6
alievaleila <meliyeva452@gmail.com>
2025-12-13 19:53:12 +0400

    initial commit

--- src/main/java/az/edu/itbrains/furni_online_store/services/impl/UserServiceImpl.java
+++ src/main/java/az/edu/itbrains/furni_online_store/services/impl/UserServiceImpl.java
@@ -1,10 +1,21 @@
 package az.edu.itbrains.furni_online_store.services.impl;
 
+import az.edu.itbrains.furni_online_store.repositories.UserRepository;
 import az.edu.itbrains.furni_online_store.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+
+
 
 
 }
