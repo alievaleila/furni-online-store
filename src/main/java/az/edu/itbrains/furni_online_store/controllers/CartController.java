d56d2f4c2fff3645a517818d8b0fcebe28e6ca9d
alievaleila <meliyeva452@gmail.com>
2026-01-02 22:05:19 +0400

    feat(cart): add increase and decrease quantity functionality

--- src/main/java/az/edu/itbrains/furni_online_store/controllers/CartController.java
+++ src/main/java/az/edu/itbrains/furni_online_store/controllers/CartController.java
@@ -18,21 +18,21 @@ public class CartController {
     private final CartService cartService;
 
     @GetMapping("/cart")
-    private String cart(Model model, Principal principal){
-        if(principal==null){
+    private String cart(Model model, Principal principal) {
+        if (principal == null) {
             return "redirect:/login";
         }
 
-        String username=principal.getName();
-        List<CartItemDto> cartItemDtoList=cartService.getCartItemByUsername(username);
-        double subtotal=cartService.calculateSubtotal(cartItemDtoList);
-        model.addAttribute("cartItems",cartItemDtoList);
+        String username = principal.getName();
+        List<CartItemDto> cartItemDtoList = cartService.getCartItemByUsername(username);
+        double subtotal = cartService.calculateSubtotal(cartItemDtoList);
+        model.addAttribute("cartItems", cartItemDtoList);
         model.addAttribute("cartTotal", subtotal);
         return "cart";
     }
 
     @PostMapping("/cart/add")
-    public String addToCart(@RequestParam Long productId,Principal principal) {
+    public String addToCart(@RequestParam Long productId, Principal principal) {
         if (principal == null) {
             return "redirect:/login";
         }
@@ -40,28 +40,23 @@ public class CartController {
 
 
     @PostMapping("/delete")
-    public String delete(@RequestParam Long productId, Principal principal){
-        if(principal==null){
-            return "redirect:/login";
-        }
-
-        String username=principal.getName();
-        cartService.deleteItem(username,productId);
+    public String delete(@RequestParam Long productId, Principal principal) {
+        String username = principal.getName();
+        cartService.deleteItem(username, productId);
         return "redirect:/cart";
     }
 
     @PostMapping("/update")
-    public String update(@RequestParam Long productId, String action, Principal principal){
-        if(principal==null){
-            return "redirect:/login";
-        }
+    public String update(@RequestParam Long productId, String action, Principal principal) {
+        String username = principal.getName();
 
-        String username=principal.getName();
-        cartService.updateQuantity(username,productId);
+        if (action.equals("increase")) {
+            cartService.increaseQuantity(username, productId);
+        } else if (action.equals("decrease")) {
+            cartService.decreaseQuantity(username, productId);
+        }
         return "redirect:/cart";
     }
-
-
 
 
 }
--- src/main/java/az/edu/itbrains/furni_online_store/services/CartService.java
+++ src/main/java/az/edu/itbrains/furni_online_store/services/CartService.java
@@ -5,7 +5,14 @@ import az.edu.itbrains.furni_online_store.dtos.CartItemDto;
 import java.util.List;
 
 public interface CartService {
+    
     List<CartItemDto> getCartItemByUsername(String username);
 
     double calculateSubtotal(List<CartItemDto> cartItemDtoList);
+
+    void deleteItem(String username, Long productId);
+
+    void increaseQuantity(String username, Long productId);
+
+    void decreaseQuantity(String username, Long productId);
 }
