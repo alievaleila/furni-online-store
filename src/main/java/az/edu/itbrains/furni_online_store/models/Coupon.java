004cdb167c5662ebb04a22cc6f4c7995f10c673d
alievaleila <meliyeva452@gmail.com>
2026-01-03 20:49:54 +0400

    refactor: update Cupon entity to use BigDecimal for precision

---
+++ src/main/java/az/edu/itbrains/furni_online_store/models/Coupon.java
@@ -0,0 +1,32 @@
+package az.edu.itbrains.furni_online_store.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="coupons")
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+    private BigDecimal discountPercentage;
+
+    private Boolean active;
+
+}
