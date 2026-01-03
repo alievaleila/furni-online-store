f7715269fc8771c937a9b86055434579d58ccee7
alievaleila <meliyeva452@gmail.com>
2026-01-03 22:34:29 +0400

    feat: implement all-contacts retrieval flow

--- src/main/java/az/edu/itbrains/furni_online_store/services/ContactService.java
+++ src/main/java/az/edu/itbrains/furni_online_store/services/ContactService.java
@@ -1,8 +1,13 @@
 package az.edu.itbrains.furni_online_store.services;
 
+import az.edu.itbrains.furni_online_store.dtos.contact.ContactDashDto;
 import az.edu.itbrains.furni_online_store.dtos.contact.ContactDto;
+
+import java.util.List;
 
 public interface ContactService {
 
     void addContact(ContactDto contactDto);
+
+    List<ContactDashDto> getAllContacts();
 }
--- src/main/java/az/edu/itbrains/furni_online_store/services/impl/ContactServiceImpl.java
+++ src/main/java/az/edu/itbrains/furni_online_store/services/impl/ContactServiceImpl.java
@@ -1,17 +1,23 @@
 package az.edu.itbrains.furni_online_store.services.impl;
 
+import az.edu.itbrains.furni_online_store.dtos.contact.ContactDashDto;
 import az.edu.itbrains.furni_online_store.dtos.contact.ContactDto;
 import az.edu.itbrains.furni_online_store.models.Contact;
 import az.edu.itbrains.furni_online_store.repositories.ContactRepository;
 import az.edu.itbrains.furni_online_store.services.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService {
 
     private final ContactRepository contactRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public void addContact(ContactDto contactDto) {
@@ -21,6 +27,13 @@ public class ContactServiceImpl implements ContactService {
         contact.setEmail(contactDto.getEmail());
         contact.setMessage(contactDto.getMessage());
         contactRepository.save(contact);
+    }
 
+    @Override
+    public List<ContactDashDto> getAllContacts() {
+        List<ContactDashDto> contactDashDtoList = contactRepository.findAll()
+                .stream()
+                .map(contact -> modelMapper.map(contact, ContactDashDto.class)).collect(Collectors.toList());
+        return contactDashDtoList;
     }
 }
